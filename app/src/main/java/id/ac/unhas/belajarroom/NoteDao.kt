a09985e2446fe4ab48c29429ce6e5cc149b407a2
FiqriA <hyugafiqri@gmail.com>
2021-05-25 22:28:37 +0800

    NoteDao

--- app/src/main/java/id/ac/unhas/belajarroom/NoteDao.kt
+++ app/src/main/java/id/ac/unhas/belajarroom/NoteDao.kt
@@ -1,5 +1,20 @@
 package id.ac.unhas.belajarroom
 
-class NoteDao {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+    @Query("Select * from note")
+    fun getNotes(): LiveData<List<Note>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
 
 }
